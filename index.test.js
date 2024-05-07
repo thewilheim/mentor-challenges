84a8d1b51654139a9086590ee05a684f418343fd
thewilheim <willabraham1@gmail.com>
2024-05-07 20:47:11 +1000

    fixed issue with test case

--- index.test.js
+++ index.test.js
@@ -1,6 +1,6 @@
 import {expect, test} from "bun:test"
-import { pascalsTriange } from '.'
+import { pascalsTriangle } from '.'
 
 test("Given an integer numRows, return the first numRows of Pascal's triangle.", () => {
-    expect(pascalsTriange(5)).toBe([[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]])
+    expect(pascalsTriangle(5)).toStrictEqual([[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]])
 })
